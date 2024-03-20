d1d05d3a9d2fe3dfb50ae22856cb01e665fdf3d9
zoedickson <zoes.account@gmail.com>
2024-03-19 20:42:32 -0500

    3.19

--- Sound/sketch.js
+++ Sound/sketch.js
@@ -1,41 +1,74 @@
-let synth = new Tone.PolySynth(Tone.Synth);
-let bend = new Tone.PitchShift();
+let img;
+let initTone = true;
+let baseFreq = 200; 
+let osc = new Tone.Oscillator(baseFreq, 'square').start();
+let gain = new Tone.Gain().toDestination();
+let pan = new Tone.Panner().connect(gain);
+let ampEnv = new Tone.AmplitudeEnvelope({
+  attack: 0.1,
+  decay: 0.5,
+  sustain: 1.0,
+  release: 1.0
+}).connect(pan);
+osc.connect(ampEnv);
 
-bend.pitch = 0;
-synth.connect(bend);
-bend.toDestination();
 
-let notes = {
-  'a' : 'C4',
-  's' : 'D4',
-  'd' : 'E4',
-  'f' : 'F4',
-  'g' : 'G4',
-  'h' : 'A4',
-  'j' : 'B4',
-  'k' : 'C5'
+let lfo = new Tone.LFO({
+  type: 'sine', 
+  frequency: 0.5, 
+  amplitude: 200, 
+}).start();
+lfo.connect(osc.frequency); 
+let noise = new Tone.Noise('pink').start();
+let noiseEnv = new Tone.AmplitudeEnvelope({
+  attack: 0.1,
+  decay: 0.8,
+  sustain: 1.0,
+  release: 0.8
+}).connect(gain);
+
+function preload(){
+  img = loadImage('assets/trombone.webp');
 }
 
 function setup() {
   createCanvas(400, 400);
-
-pitchSlider = createSlider(-12, 12, 0, 0.1); //pitch down -12, pitch up 12, starting point is 0
-pitchSlider.position (120, 200);
-pitchSlider.mouseMoved(()=> bend.pitch = pitchSlider.value());
-
-}
-
-function keyPressed(){
-  let playNotes = notes[key];
-  synth.triggerAttack(playNotes);
-}
-
-function keyReleased(){
-  let playNotes = notes[key];
-  synth.triggerRelease(playNotes,'+0.03');
 }
 
 function draw() {
-  background('pink');
-  text ('play A-K for synth', 140, 180)
+  background('black');
+  if(mouseIsPressed){
+    background(img);
+  }
+  fill(250);
+  text('press to start trombone',130,50);
 }
+
+function keyPressed() {
+  if (keyCode === 32 && initTone === true) {
+    console.log('spacebar pressed');
+
+    Tone.start();
+    initTone = false;
+  }
+}
+
+function mousePressed() {
+ 
+  let notes = ['D4', 'C#4', 'C4', 'B3'];
+
+  let lastNoteRelease = '8n';
+
+
+  notes.forEach((note, index) => {
+    let time = index * 0.8; 
+    osc.frequency.setValueAtTime(Tone.Frequency(note).toFrequency(), Tone.now() + time);
+    ampEnv.triggerAttackRelease('6n', Tone.now() + time);
+  });
+
+ 
+  let freq = map(mouseX, 0, width, 0.1, 10); 
+  lfo.frequency.value = freq;
+}
+
+
